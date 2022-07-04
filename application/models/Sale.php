003d2307d827c2f4ff77d050c65b1cac714957ce
vzegers <vicente.zegers@hotmail.com>
2022-07-04 17:23:40 -0400

    Agregado ventas masivas

--- application/models/Sale.php
+++ application/models/Sale.php
@@ -1,10 +1,7 @@
 <?php
-    require_once('traits/uniquecheck.php');
-    class Sale extends ActiveRecord\Model {
 
-        public static $table_name = 'zarest_sales';
-        use uniquecheck;
-        public function validate() {
-            $this->uniquecheck(array('code','message' => 'Can\'t have duplicate code.'));
-        }
-    }
+class Sale extends ActiveRecord\Model {
+
+   public static $table_name = 'zarest_sales';
+
+}
